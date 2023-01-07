445d89fb965c2e87a04554d62334848516e02a59
Mark <markfamiliytitan@gmail.com>
2023-01-07 16:19:42 +0200

    li

--- src/components/Main/Elements/elementsExperience.js
+++ src/components/Main/Elements/elementsExperience.js
@@ -1,4 +1,3 @@
-
 export const Experience = ({ theme, experience }) => {
   return (
     <div
@@ -6,22 +5,28 @@ export const Experience = ({ theme, experience }) => {
       id="experience"
       className={`main_item experience experience_${theme} ${theme}`}
     >
-      {experience.map((element) => (
-        <div
-          id={`experienceEl`}
-          className={`experienceElItems experienceEl_${theme}`}
-        >
-           {Object.keys(element).map((key) => (
-           <div
-             id={`Item_${key}`}
-             key={key}
-             className={`${key}Items ${key}Items_${key} ${key}Items_${theme}`}
-           >
-             {element[key]}
-          </div>
-         ))}
-        </div>
-      ))}
+      <ul
+        id={`experience`}
+        key={experience}
+        className={`experience experience_${theme}`}
+      >
+        {experience.map((element) => (
+          <li
+            id={`experienceEl`}
+            className={`experienceElItems experienceEl_${theme}`}
+          >
+            {Object.keys(element).map((key) => (
+              <div
+                id={`Item_${key}`}
+                key={key}
+                className={`${key}Items ${key}Items_${key} ${key}Items_${theme}`}
+              >
+                {element[key]}
+              </div>
+            ))}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
